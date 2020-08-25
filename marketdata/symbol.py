e20cacda41e732b5511103692265c2c29aaf1cfe
Lance Haynie <lance@hayniemail.com>
2020-08-25 17:25:47 -0600

    updates

--- marketdata/symbol.py
+++ marketdata/symbol.py
@@ -21,6 +21,7 @@
 #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #See the License for the specific language governing permissions and
 #limitations under the License.
+import sys
 import pandas as pd
 from .settings import settings_data
 from .database import db
